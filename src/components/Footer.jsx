245dc5db24bea7c14ec26d052b21d18745b790c1
Roeck <rons9n@gmail.com>
2020-04-15 19:56:44 -0400

    refactoring

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
+
+const currentYear = new Date().getFullYear();
 
 function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer>
-      <p>Copyright ©{currentYear}</p>
+      <p> ©Copyright {currentYear} </p>{' '}
     </footer>
   );
 }
